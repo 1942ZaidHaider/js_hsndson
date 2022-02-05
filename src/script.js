dc706905e49e7eaad4b4cc5eaa0ba3d6fdc5b1f0
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 11:58:01 +0530

    String blanks

--- src/script.js
+++ src/script.js
@@ -3,11 +3,15 @@ function print(str){
     str="Output: <br><br>"+str;
     out.innerHTML=str;
 }
-var x = 'hello';
-var y =' world';
-// x[0]="H" Error
-x='Hello';
-output=`3rd to char of ${x} = ${x[x.length-3]}`;
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The "+myAdjective+" "+myNoun+" "+myVerb+" "+myAdverb; // Change this line
+// Only change code above this line
+output=wordBlanks;//`3rd to char of ${x} = ${x[x.length-3]}`;
 
 
 console.log(output);
