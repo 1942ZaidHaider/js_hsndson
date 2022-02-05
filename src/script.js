289dfd608741cef18120115abbfd5e3bc7fc198a
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 14:08:37 +0530

    Returing boolean

--- src/script.js
+++ src/script.js
@@ -4,27 +4,13 @@ function print(str) {
   str = "Output: <br><br>" + str;
   out.innerHTML = str;
 }
-function chainToSwitch(val) {
-    let answer = "";
+
+function isLess(a, b) {
     // Only change code below this line
-  switch(val){
-    case "bob":  {
-      answer = "Marley"; break;
-    } case 42: {
-      answer = "The Answer"; break;
-    } case 1: {
-      answer = "There is no #1"; break;
-    } case 99: {
-      answer = "Missed me by this much!"; break;
-    } case 7: {
-      answer = "Ate Nine"; break;
-    }
-    default: answer=""; break;
-  }
+    return a<b;
     // Only change code above this line
-    return answer;
   }
   
-  output=chainToSwitch(7);
+  output=isLess(10, 15); 
 // output = `${testLogicalAnd(10)}`;
 onload = print(output); //Output
