f5a5cf4a701ce2222e63098c13b0b81a95d8209d
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 15:19:44 +0530

    for loop

--- src/script.js
+++ src/script.js
@@ -28,10 +28,12 @@ var recordCollection = {
   // Only change code below this line
 // Setup
 const myArray = [];
-let i=5;
-while(i >= 0){
-  myArray[myArray.length]=i--;
-} 
+for(let i=1;i<6;i++)
+{
+  myArray[myArray.length]=i
+}
+// Only change code below this line
+
 // Only change code below this line
   output=`${myArray}`;
 onload = print(output); //Output
