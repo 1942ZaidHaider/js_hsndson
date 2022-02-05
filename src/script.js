6af7995358e6740498b9934d12b2ec6d2a760eb0
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 11:23:55 +0530

    Compound add

--- src/script.js
+++ src/script.js
@@ -3,7 +3,10 @@ function print(str){
     str="Output: <br><br>"+str;
     out.innerHTML=str;
 }
-const x = 43;
-var y = 9;
-output=`${x} % ${y} = ${x%y}`;
-onload=print(output);
+var x = 43;
+x += 5;
+output=`x=43 => x+=5 => ${x}`;
+
+
+
+onload=print(output); //Output
