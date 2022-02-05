61a2a348c36cb46165245b33253cc7a984944b0a
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 13:43:25 +0530

    else if chain

--- src/script.js
+++ src/script.js
@@ -4,16 +4,22 @@ function print(str) {
   str = "Output: <br><br>" + str;
   out.innerHTML = str;
 }
-function orderMyLogic(val) {
-  if (val < 5) {
-    return "Less than 5";
-  } else if (val < 10) {
-    return "Less than 10";
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
   } else {
-    return "Greater than or equal to 10";
+    return "Huge";
   }
+  // Only change code above this line
 }
 
-output = orderMyLogic(7);
+output = testSize(7);
 // output = `${testLogicalAnd(10)}`;
 onload = print(output); //Output
