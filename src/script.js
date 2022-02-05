0e9e3871cc26811e302a98de22ec2947fabc37db
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 13:35:51 +0530

    else if

--- src/script.js
+++ src/script.js
@@ -4,22 +4,16 @@ function print(str) {
   str = "Output: <br><br>" + str;
   out.innerHTML = str;
 }
-function testElse(val) {
-    let result = "";
-    // Only change code below this line
-  
-    if (val > 5) {
-      result = "Bigger than 5";
-    }
-  
-    else{
-      result = "5 or Smaller";
-    }
-  
-    // Only change code above this line
-    return result;
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
   }
-  
-  output=testElse(4); 
+}
+
+output = testElseIf(7);
 // output = `${testLogicalAnd(10)}`;
 onload = print(output); //Output
