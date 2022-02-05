6334a8c0f1d98dfdb9d20b1f2f99b5f7a7f67c73
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 12:03:26 +0530

    nested Array

--- src/script.js
+++ src/script.js
@@ -3,8 +3,8 @@ function print(str){
     str="Output: <br><br>"+str;
     out.innerHTML=str;
 }
-const ar=["dog","cat","pokemon"];
-output=`${ar} <br> type: ${typeof(ar)}`;
+const ar=["dog","cat","pokemon",["raichu","aabra","Garados"]];
+output=`${ar} <br>\n type of ar.length-1: ${typeof(ar[ar.length-1])}`;
 
 
 console.log(output);
