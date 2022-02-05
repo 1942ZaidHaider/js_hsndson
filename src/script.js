05b82e7ba64f093b8a8dfb3b0a11d8e6b123490d
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 13:14:52 +0530

    not equal

--- src/script.js
+++ src/script.js
@@ -5,13 +5,14 @@ function print(str) {
   out.innerHTML = str;
 }
 // Setup
-// Setup
-function compareEquality(a, b) {
-    if (a === b) { // Change this line
-      return "Equal";
+function testNotEqual(val) {
+    if (val != 99) { // Change this line
+      return "Not Equal";
     }
-    return "Not Equal";
+    return "Equal";
   }
   
-output = `${compareEquality(10,"10")}<br>\t${compareEquality("12",12)}<br>\t${compareEquality(12,12)}`; //oops global cannot be accessed because it is local to fun1
+
+  
+output = `${testNotEqual(10)}<br>\n${testNotEqual(99)}<br>\n${testNotEqual("12")}`; //oops global cannot be accessed because it is local to fun1
 onload = print(output); //Output
