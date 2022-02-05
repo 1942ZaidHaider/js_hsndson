97a8a055081f3a95b969072040b68d04b53de99e
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 12:13:31 +0530

    Push

--- src/script.js
+++ src/script.js
@@ -4,7 +4,8 @@ function print(str){
     out.innerHTML=str;
 }
 const ar=["dog","cat","pokemon",["raichu","aabra","Garados"]];
-output=`ar[3][0] ${ar[3][0]}<br>\nar[3][1] ${ar[3][1]}<br>\n`;
+ar.push("Fluffy")
+output=`pushed Fluffy: ${ar}`;
 
 
 console.log(output);
