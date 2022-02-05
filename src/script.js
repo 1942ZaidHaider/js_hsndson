0ab800a377ccec5232dc3948a0536646e7329dbe
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 13:04:48 +0530

    boolean with conditonals

--- src/script.js
+++ src/script.js
@@ -4,14 +4,15 @@ function print(str) {
   str = "Output: <br><br>" + str;
   out.innerHTML = str;
 }
-
-function welcomeToBooleans() {
+function trueOrFalse(wasThatTrue) {
     // Only change code below this line
-  
-    return true; // Change this line
-  
+    if(wasThatTrue)
+        return "Yes, That was true";
+    else
+        return "No, That was false";  
     // Only change code above this line
+  
   }
 
-output = `${welcomeToBooleans()}`; //oops global cannot be accessed because it is local to fun1
+output = `${trueOrFalse(true)}<br>\t${trueOrFalse(false)}`; //oops global cannot be accessed because it is local to fun1
 onload = print(output); //Output
