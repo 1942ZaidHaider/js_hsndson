3b5412e277de166671e0406c55a279de382e9ef3
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 13:52:10 +0530

    Switch case

--- src/script.js
+++ src/script.js
@@ -4,34 +4,41 @@ function print(str) {
   str = "Output: <br><br>" + str;
   out.innerHTML = str;
 }
-const names = [
-  "Hole-in-one!",
-  "Eagle",
-  "Birdie",
-  "Par",
-  "Bogey",
-  "Double Bogey",
-  "Go Home!",
-];
-
-function golfScore(par, strokes) {
-  if (strokes == 1) {
-    return "Hole-in-one!";
-  } else if (strokes <= par - 2) {
-    return "Eagle";
-  } else if (strokes == par - 1) {
-    return "Birdie";
-  } else if (strokes == par) {
-    return "Par";
-  } else if (strokes == par + 1) {
-    return "Bogey";
-  } else if (strokes == par + 2) {
-    return "Double Bogey";
-  } else {
-    return "Go Home!";
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      {
+        answer = "alpha";
+      }
+      break;
+    case 2:
+      {
+        answer = "beta";
+      }
+      break;
+    case 3:
+      {
+        answer = "gamma";
+      }
+      break;
+    case 4:
+      {
+        answer = "delta";
+      }
+      break;
+    default:
+      {
+        answer = "unknown";
+      }
+      break;
   }
+
+  // Only change code above this line
+  return answer;
 }
 
-output = golfScore(5, 4);
+output = caseInSwitch(1);
 // output = `${testLogicalAnd(10)}`;
 onload = print(output); //Output
