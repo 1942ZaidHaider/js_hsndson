3a99094ad848bd5f0e86239507c33678f37bfde5
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 13:19:45 +0530

    Grater than

--- src/script.js
+++ src/script.js
@@ -4,15 +4,17 @@ function print(str) {
   str = "Output: <br><br>" + str;
   out.innerHTML = str;
 }
-// Setup
-function testNotEqual(val) {
-    if (val !== 99) { // Change this line
-      return "Not Equal";
+function testGreaterThan(val) {
+    if (val > 100) {  // Change this line
+      return "Over 100";
     }
-    return "Equal";
+  
+    if (val > 10) {  // Change this line
+      return "Over 10";
+    }
+  
+    return "10 or Under";
   }
-  
-
-  
-output = `${testNotEqual(10)}<br>\n${testNotEqual(99)}<br>\n${testNotEqual("99")}`; //oops global cannot be accessed because it is local to fun1
+    
+output = `${testGreaterThan(10)}`; //oops global cannot be accessed because it is local to fun1
 onload = print(output); //Output
