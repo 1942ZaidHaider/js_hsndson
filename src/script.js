36b5952b19d42079f81cb48d112013de9537dab4
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 13:59:59 +0530

    Switch case range

--- src/script.js
+++ src/script.js
@@ -4,20 +4,24 @@ function print(str) {
   str = "Output: <br><br>" + str;
   out.innerHTML = str;
 }
-function switchOfStuff(val) {
+function sequentialSizes(val) {
     let answer = "";
     // Only change code below this line
     switch(val){
-      case 'a': answer="apple"; break;
-      case 'b': answer="bird"; break;
-      case 'c': answer="cat"; break;
-      default: answer="stuff"; break;
+      case 1: case 2: 
+      case 3: answer="Low"; break;
+      case 4: case 5: 
+      case 6: answer="Mid"; break;
+      case 7: case 8: 
+      case 9: answer="High"; break;
+    default : answer="err";
     }
   
   
     // Only change code above this line
     return answer;
   }
-output = switchOfStuff("a");
+  
+  output=sequentialSizes(1);
 // output = `${testLogicalAnd(10)}`;
 onload = print(output); //Output
