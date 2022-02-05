9b7a951575154dec73391b4e65b7328a8ad4505a
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 12:00:47 +0530

    Array

--- src/script.js
+++ src/script.js
@@ -3,15 +3,8 @@ function print(str){
     str="Output: <br><br>"+str;
     out.innerHTML=str;
 }
-const myNoun = "dog";
-const myAdjective = "big";
-const myVerb = "ran";
-const myAdverb = "quickly";
-
-// Only change code below this line
-const wordBlanks = "The "+myAdjective+" "+myNoun+" "+myVerb+" "+myAdverb; // Change this line
-// Only change code above this line
-output=wordBlanks;//`3rd to char of ${x} = ${x[x.length-3]}`;
+const ar=["dog","cat","pokemon"];
+output=`${ar} <br> type: ${typeof(ar)}`;
 
 
 console.log(output);
