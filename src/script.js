96557f8bc0eb5bb41d326a314de122dd7ec561eb
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 15:23:45 +0530

    reverse Odd numbers

--- src/script.js
+++ src/script.js
@@ -6,30 +6,31 @@ function print(str) {
 }
 // Setup
 var recordCollection = {
-    2548: {
-      albumTitle: 'Slippery When Wet',
-      artist: 'Bon Jovi',
-      tracks: ['Let It Rock', 'You Give Love a Bad Name']
-    },
-    2468: {
-      albumTitle: '1999',
-      artist: 'Prince',
-      tracks: ['1999', 'Little Red Corvette']
-    },
-    1245: {
-      artist: 'Robert Palmer',
-      tracks: []
-    },
-    5439: {
-      albumTitle: 'ABBA Gold'
-    }
-  };
-  
-  // Only change code below this line
+  2548: {
+    albumTitle: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    albumTitle: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    albumTitle: "ABBA Gold",
+  },
+};
+
+// Only change code below this line
 // Setup
 const myArray = [];
-for(let i=1;i<10;i+=2){
+for (let i = 9; i > 0; i -= 2) {
   myArray.push(i);
 }
-  output=`${myArray}`;
+
+output = `${myArray}`;
 onload = print(output); //Output
