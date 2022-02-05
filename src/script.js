76610bbe2c69090c8dee7d35b7afb2a0dd2e5981
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 12:40:44 +0530

    Function and local scope

--- src/script.js
+++ src/script.js
@@ -5,12 +5,11 @@ function print(str) {
   out.innerHTML = str;
   }
 
-  // Declare the myGlobal variable below this line
-var myGlobal=10;
-
-function fun1() {
-  // Assign 5 to oopsGlobal Here
-  oopsGlobal=5;
-}
-output = `${myGlobal}`; //oops global cannot be accessed because it is local to fun1
+  function myLocalScope() {
+    // Only change code below this line
+    myVar=12;
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+output = `Refer to console`; //oops global cannot be accessed because it is local to fun1
 onload = print(output); //Output
