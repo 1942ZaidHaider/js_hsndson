a8e165e0a3a8e9170741dade15796b379a3056d5
cedcoss <zaidhaiderrizvi@cedcoss.com>
2022-02-05 13:28:46 +0530

    AND

--- src/script.js
+++ src/script.js
@@ -4,18 +4,15 @@ function print(str) {
   str = "Output: <br><br>" + str;
   out.innerHTML = str;
 }
-function testLessOrEqual(val) {
-    if (val <= 12 ) {  // Change this line
-      return "Smaller Than or Equal to 12";
-    }
+function testLogicalAnd(val) {
+    // Only change code below this line
   
-    if (val<=24) {  // Change this line
-      return "Smaller Than or Equal to 24";
-    }
-  
-    return "More Than 24";
+    if (val <= 50 && val > 24) {
+      return "Yes";    
+    }  // Only change code above this line
+    return "No";
   }
   
-  testLessOrEqual(10);  
-output = `${testLessOrEqual(10)}`; //oops global cannot be accessed because it is local to fun1
+  testLogicalAnd(10);  
+output = `${testLogicalAnd(10)}`; //oops global cannot be accessed because it is local to fun1
 onload = print(output); //Output
